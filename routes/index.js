a71db778f75702ba56d7f95e3d6c425d39232e13
Andrew Becker <andrewjoelbecker@gmail.com>
2017-03-12 00:01:12 -0700

    Added mongo db and basic library

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
+var library = require('library');
+
 var projectionEndpoint = 'https://data.colorado.gov/resource/ba4c-qx73.json'
 
 /* GET home page. */
@@ -9,6 +11,19 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/role/new', function(req, res, next) {
+	var role = new library.Role("Test", 100000);
+	console.log(role);
+	role.save();
+	res.render('index');
+});
+
+router.get('/role', function(req, res, next){
+	library.Role.Find().then(function(roles){
+		res.setHeader('Content-Type', 'application/json');
+    	res.send(roles);
+	})
+})
 
 
 router.get('/projections', function(req, res, next){
