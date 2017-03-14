21e34d1b2ed22786b89aa7eb690d8ac011ac6f2f
Andrew Becker <andrewjoelbecker@gmail.com>
2017-03-13 18:45:59 -0600

    Cleaned some things up and changed post method to look at request body

--- routes/index.js
+++ routes/index.js
@@ -11,19 +11,20 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/role/new', function(req, res, next) {
-	var role = new library.Role("Test", 100000);
-	console.log(role);
-	role.save();
-	res.render('index');
-});
-
 router.get('/role', function(req, res, next){
 	library.Role.Find().then(function(roles){
 		res.setHeader('Content-Type', 'application/json');
-    	res.send(roles);
+    res.send(roles);
 	})
 })
+
+router.post('/role/new', function(req, res, next) {
+	var roleParams = req.body.Role;
+	var role = new library.Role(roleParams.name, roleParams.salary);
+	console.log(roleParams);
+	role.save();
+	res.render('index');
+});
 
 
 router.get('/projections', function(req, res, next){
