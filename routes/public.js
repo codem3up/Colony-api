0ec50e9162db2bd899b5acda090ba579287dc02c
Andrew Becker <andrewjoelbecker@gmail.com>
2017-03-21 20:44:40 -0600

    Added more necessary endpoints

--- routes/public.js
+++ routes/public.js
@@ -1,11 +1,21 @@
 module.exports = function(router){
 	
 	var projectionEndpoint = 'https://data.colorado.gov/resource/ba4c-qx73.json'
+	var occupationWagesEndpoint = ''
 	var request = require('request');
-
 
 	router.get('/projections', function(req, res, next){
 		request(projectionEndpoint, function (error, response, body) {
+			if(error) {
+				res.status(500).send(error);
+			} 
+	    	res.setHeader('Content-Type', 'application/json');
+	    	res.send(body);
+		});
+	})
+
+	router.get('/user/:id/comparesalaries', function(req, res, next){
+		request(occupationWagesEndpoint, function (error, response, body) {
 			if(error) {
 				res.status(500).send(error);
 			} 
--- routes/team.js
+++ routes/team.js
@@ -12,6 +12,21 @@ module.exports = function(router){
 		})
 	})
 
+	router.get('/member/:id', function(req, res, next){
+		res.setHeader('Content-Type', 'application/json');
+		//Check if valid object id before lookup
+		if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
+			res.send({});
+		} else{
+			library.TeamMember.Find({ _id: req.params.id }).then(function(member){
+				console.log("member", member);
+		    	res.send(member);
+			}).catch(function(err){
+				res.send(err);
+			})
+		}
+	})
+
 
 	router.get('/user/:id/member/all', function(req, res, next){
 		res.setHeader('Content-Type', 'application/json');
@@ -39,8 +54,11 @@ module.exports = function(router){
 		}
 	})
 
+
+
 	router.post('/user/:id/member/new', function(req, res, next){
 		res.setHeader('Content-Type', 'application/json');
+
 
 		var teamParams = req.body.teamMember;
 
@@ -63,7 +81,7 @@ module.exports = function(router){
 								res.sendStatus(200);
 							}).catch(function(err){
 								res.sendStatus(404);
-							});
+						});
 		    	}
 		    	//User not found
 		    	else{
@@ -75,6 +93,32 @@ module.exports = function(router){
 		} else {
 			res.sendStatus(404)
 		}
+	})
+
+	router.get('/user/:id/member/:memberid', function(req, res, next){
+		res.setHeader('Content-Type', 'application/json');
+		//Check if valid object id before lookup
+		if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
+			res.send({});
+		} else{
+			library.TeamMember.Find({ _id: req.params.memberid, userId: req.params.id }).then(function(member){
+				console.log("member", member);
+		    	res.send(member);
+			}).catch(function(err){
+				res.send(err);
+			})
+		}
+	})
+
+	router.post('/user/:id/member/delete', function(req, res, next) {
+		res.setHeader('Content-Type', 'application/json');
+
+		library.TeamMember.Delete({ id: req.params.id }).then(function(){
+			res.sendStatus(200);
+		}).catch(function(){
+			console.log(result);
+			res.sendStatus(404);
+		})
 	})
 
 
