cfce32250d2a497c6358ce7c0bf9d5ca7d557011
Andrew Becker <andrewjoelbecker@gmail.com>
2017-04-08 12:30:11 -0600

    Added documentation endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-require('./user')(router);
-require('./public_occupation')(router);
-require('./team_member')(router);
-require('./personal_income')(router);
-
-
 /* GET home page. */
 router.get('/', (req, res, next) => {
 	res.render('index');
 });
 
+router.get('/api/docs', (req, res, next) => {
+	res.render('documentation');
+})
+
+require('./user')(router);
+require('./public_occupation')(router);
+require('./team_member')(router);
+require('./personal_income')(router);
 
 module.exports = router;
 
